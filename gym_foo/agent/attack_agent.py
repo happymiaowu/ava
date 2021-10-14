496236233f9e2d3acb59f349d42005aea5b7f6df
tianhe1986 <w1s2j3229@163.com>
2021-10-14 18:53:57 +0800

    1. Fix nearest points. 2.  Add laughing action.
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- gym_foo/agent/attack_agent.py
+++ gym_foo/agent/attack_agent.py
@@ -301,8 +301,8 @@ class TowerRushMoreRangeWeightAgent(Agent):
     now_index = 0
 
     # 离主塔最近的点
-    nearest_list = [(5, 6), (7, 6), (6, 5), (6, 7), (5, 5), (5, 7), (7, 5), (7, 7), (4, 6), (8, 6), (6, 4), (6, 8), (5, 4), (5, 6), (7, 4), (7, 6), (4, 5), (4, 7), (6, 5), (6, 7)]
-    nearest_set = set(((5, 6), (7, 6), (6, 5), (6, 7), (5, 5), (5, 7), (7, 5), (7, 7), (4, 6), (8, 6), (6, 4), (6, 8), (5, 4), (5, 6), (7, 4), (7, 6), (4, 5), (4, 7), (6, 5), (6, 7)))
+    nearest_list = [(5, 6), (7, 6), (6, 5), (6, 7), (5, 5), (5, 7), (7, 5), (7, 7), (4, 6), (8, 6), (6, 4), (6, 8), (5, 4), (5, 8), (7, 4), (7, 8), (4, 5), (4, 7), (8, 5), (8, 7)]
+    nearest_set = set(((5, 6), (7, 6), (6, 5), (6, 7), (5, 5), (5, 7), (7, 5), (7, 7), (4, 6), (8, 6), (6, 4), (6, 8), (5, 4), (5, 8), (7, 4), (7, 8), (4, 5), (4, 7), (8, 5), (8, 7)))
 
     # 行军数量是否满
     def is_march_num_full(self, hive):
@@ -377,10 +377,11 @@ class TowerRushOptimalAgent(Agent):
 
     valid_action = ['attack', 'reinforce', 'rally', 'scout', 'speedup', 'teleport', 'recall_march', 'recall_occupied', 'join_rally', 'cancel_rally']
     now_index = 0
+    has_laugh = False
 
     # 离主塔最近的点
-    nearest_list = [(5, 6), (7, 6), (6, 5), (6, 7), (5, 5), (5, 7), (7, 5), (7, 7), (4, 6), (8, 6), (6, 4), (6, 8), (5, 4), (5, 6), (7, 4), (7, 6), (4, 5), (4, 7), (6, 5), (6, 7)]
-    nearest_set = set(((5, 6), (7, 6), (6, 5), (6, 7), (5, 5), (5, 7), (7, 5), (7, 7), (4, 6), (8, 6), (6, 4), (6, 8), (5, 4), (5, 6), (7, 4), (7, 6), (4, 5), (4, 7), (6, 5), (6, 7)))
+    nearest_list = [(5, 6), (7, 6), (6, 5), (6, 7), (5, 5), (5, 7), (7, 5), (7, 7), (4, 6), (8, 6), (6, 4), (6, 8), (5, 4), (5, 8), (7, 4), (7, 8), (4, 5), (4, 7), (8, 5), (8, 7)]
+    nearest_set = set(((5, 6), (7, 6), (6, 5), (6, 7), (5, 5), (5, 7), (7, 5), (7, 7), (4, 6), (8, 6), (6, 4), (6, 8), (5, 4), (5, 8), (7, 4), (7, 8), (4, 5), (4, 7), (8, 5), (8, 7)))
 
     # 行军数量是否满
     def is_march_num_full(self, hive):
@@ -396,6 +397,51 @@ class TowerRushOptimalAgent(Agent):
         empty_cord -= set([tower.get_cord() for tower in towers])
         return empty_cord
 
+    def laugh_action(self, state, score, detail: BattleField):
+        action_list = []
+        if self.has_laugh:
+            return ActionList(team=self._team, action_list=action_list)
+        
+        my_hives = detail.get_self_hives()
+        can_lauth = True
+
+        
+        # 必须都剩名额
+        for hive in my_hives:
+            if hive.get_teleport_times() == 0:
+                can_lauth = False
+                break
+        
+        if not can_lauth:
+            return ActionList(team=self._team, action_list=action_list)
+
+        # 必须点为空
+        # laugh_points = [(8, 1), (9, 0), (10, 1), (6, 3), (7, 4), (8, 3), (10, 3), (11, 4), (12, 3)]
+        laugh_points = [(9, 1), (10, 0), (11, 1), (12, 2), (11, 3), (10, 2), (9, 3), (8, 2), (0, 12)]
+        empty_cord = self.get_empty_cord(detail.get_width(), detail.get_height(), detail.get_self_hives(), detail.get_opp_hives(), detail.get_towers())
+        for laugh_point in laugh_points:
+            if laugh_point not in empty_cord:
+                can_lauth = False
+                break
+        
+        if not can_lauth:
+            return ActionList(team=self._team, action_list=action_list)
+
+        self.has_laugh = True
+
+        
+        i = 0
+        for hive in my_hives:
+            action_list.append(
+                Teleport(
+                    hive_id=hive.get_id(),
+                    new_cord=laugh_points[i]
+                )
+            )
+            i += 1
+
+        return ActionList(team=self._team, action_list=action_list)
+
     def generate_action(self, state, score, detail: BattleField):
         action_list = []
         tower = detail.get_towers()
@@ -410,6 +456,14 @@ class TowerRushOptimalAgent(Agent):
 
         my_hives = detail.get_self_hives()
         my_hives = sorted(my_hives, key=lambda s: s.get_buff(), reverse=True)
+
+        # 已经赢了，开嘲讽
+        round_total_scores = 0
+        for tower_item in tower:
+            round_total_scores += tower_item.get_score_per_round()
+
+        if detail.get_score()[detail._team] - detail.get_score()[detail._other_team] > round_total_scores * detail.get_round_left():
+            return self.laugh_action(state, score, detail)
 
         my_marches = detail.get_self_marches()
         for hive in my_hives:
